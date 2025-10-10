08bd93029f42cd68116132a2abe1a28e534e76b5
julian.christ <julian.christ@gmail.com>
2025-10-10 18:17:17 +0000

    Separate e2e config

---
+++ vitest.e2e.config.ts
@@ -0,0 +1,23 @@
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  test: {
+    include: [
+      'tests/e2e/**/*.{test,spec}.ts',
+    ],
+    browser: {
+      enabled: true,
+      instances: [
+        { browser: 'chromium' },
+      ],
+    },
+  },
+})
