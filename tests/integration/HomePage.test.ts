22c8140e48bd6fc0702eb947eb0dda3679c1da2c
julian.christ <julian.christ@gmail.com>
2025-11-11 17:58:35 +0000

    Fixed test

--- tests/integration/HomePage.test.ts
+++ tests/integration/HomePage.test.ts
@@ -5,7 +5,7 @@ import HomePage from '@/pages/HomePage.vue'
 describe('HomePage', () => {
   it('displays proper message', () => {
     const wrapper = mount(HomePage)
-    expect(wrapper.html()).toContain('Home Page')
+    expect(wrapper.html()).toContain('Hello World')
     // expect(wrapper.html()).toMatchSnapshot();
   })
 })
